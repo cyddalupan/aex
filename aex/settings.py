58748adbd16f2795d23d0adeb644766c07dea65c
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-01-02 19:06:41 +0800

    use lite sql on dev

--- aex/settings.py
+++ aex/settings.py
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'aex.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aex',
-        'USER': 'root',
-        'PASSWORD': '3#eE4$rR5%tT',
-        'HOST': 'localhost',
-        'PORT': '3306',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'aex',
+    #     'USER': 'root',
+    #     'PASSWORD': '3#eE4$rR5%tT',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
