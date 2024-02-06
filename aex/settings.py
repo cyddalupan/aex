e7918bad8ba523d0224c2c57eba1e0b34b52c606
Cyd Dalupan <cydmdalupan@gmail.com>
2024-02-06 00:50:46 +0000

    Adding google login

--- aex/settings.py
+++ aex/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
-    'login.apps.LoginConfig'
+    'login.apps.LoginConfig',
+    'content.apps.ContentConfig',
 ]
 
 MIDDLEWARE = [
--- aex/urls.py
+++ aex/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('', include('dashboard.urls')),
+    path('content/', include('content.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
