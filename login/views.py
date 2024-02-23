428e9146f639958dad3d7a07a26c0a46d0c579a6
Cyd Dalupan <cydmdalupan@gmail.com>
2024-02-23 06:44:55 +0000

    failed socmed login

--- login/views.py
+++ login/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def login(request):
   return render(request, 'login/login.html')
+
+@csrf_exempt
+def googleLogin(request):
+  return render(request, 'login/google_login.html')
