c63c5410e493c00cd15f236aea3e57bc6e7fa75e
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-02-24 13:16:18 +0800

    done research for captcha

--- login/views.py
+++ login/views.py
@@ -1,9 +1,19 @@
+from venv import logger
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from captcha.fields import CaptchaField
+
+from .forms import ContactForm
 
 # Create your views here.
 def login(request):
-  return render(request, 'login/login.html')
+  if request.method == 'POST':
+      form = ContactForm(request.POST)
+      if form.is_valid():
+        print("Form data: %s", form.cleaned_data)
+  else:
+      form = ContactForm()
+  return render(request, 'login/login.html', {'form': form})
 
 @csrf_exempt
 def googleLogin(request):
