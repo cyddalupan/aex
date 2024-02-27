8975effab6f5a3e23c251f05dc88984ee0130581
cyd dalupan <cydmdalupan@gmail.com>
2024-02-27 08:17:25 +0000

    Add Bootstrap

--- aex/settings.py
+++ aex/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,7 +131,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/' 
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static')),
+    str(BASE_DIR.joinpath('node_modules/bootstrap/dist/')),
+    str(BASE_DIR.joinpath('node_modules/@fortawesome/fontawesome-free/')),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
