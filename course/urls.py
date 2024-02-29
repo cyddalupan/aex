b37841bbcafe3fb68972475c8e7e3dc925b0c138
cyd dalupan <cydmdalupan@gmail.com>
2024-02-29 05:58:27 +0000

    Edit Course

--- course/urls.py
+++ course/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('list', views.list, name='course-list'),
     path('add', views.add, name='course-add'),
+    path('edit/<int:course_id>/', views.edit, name='course-edit'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from course.forms import CourseForm
+from .forms import CourseForm
 from course.models import Course
 from login.models import EmailUser
 
@@ -32,4 +32,22 @@ def add(request):
       return redirect(reverse('course-list'))
   else:
     form = CourseForm()
+  return render(request, 'course/add.html', {'form': form})
+def edit(request, course_id):
+  if not checkLogin(request):
+      return redirect(reverse('error-message'))
+  instance = get_object_or_404(Course, pk=course_id)
+  if request.method == 'POST':
+    form = CourseForm(request.POST)
+    if form.is_valid():
+      instance.name = form.cleaned_data['name']
+      instance.description = form.cleaned_data['description']
+      instance.save()
+      return redirect(reverse('course-list'))
+  else:
+    # Initialize the form with the instance data
+    form = CourseForm(initial={
+      'name': instance.name,
+      'description': instance.description,
+    })
   return render(request, 'course/add.html', {'form': form})
